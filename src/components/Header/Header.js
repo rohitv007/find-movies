af456ec6793c76a9d5089fa4f19c53f826e98e26
ROHIT VIJAYVARGIYA <66072894+rohitv007@users.noreply.github.com>
2024-07-12 12:13:40 +0000

    update: header svgs

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import RMDBLogo from "../../images/react-movie-logo.svg";
-import TMDBLogo from "../../images/tmdb_logo.svg";
-import { Content, LogoImg, TMDBLogoImg, Wrapper } from "./Header.styles";
+import { Content, LogoImg, Wrapper } from "./Header.styles";
 
 const Header = () => {
   return (
@@ -11,7 +10,6 @@ const Header = () => {
         <Link to="/">
           <LogoImg src={RMDBLogo} alt="rmdb_logo" />
         </Link>
-        <TMDBLogoImg src={TMDBLogo} alt="tmdb-logo" />
       </Content>
     </Wrapper>
   );
